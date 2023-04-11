f85e1a1ec3376bf4469916a358736423adb05de8
Ronnie Odom <ronnieo45@yahoo.com>
2023-04-11 08:42:43 -0400

    first commit

---
+++ my_prework.py
@@ -0,0 +1,15 @@
+# Question 1:
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function.
+
+def hello_name(username):
+    # Your code here.
+    pass
+
+# Question 2:
+# Write a python funtion, first_odds that prints the odd numbers from 1-100 and returns nothing.
+def first_odds():
+    # Your code here.
+    pass
+
+# etc., etc., etc...
+
